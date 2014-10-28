79f81de29a5873a94484d04923fa11a959dc2936
Krishna <radhakrishana@hotmail.com>
2014-10-28 15:30:44 +0530

    ask oracle

--- themes/ahead_bazaar_child/ask_oracle - Copy.php
+++ themes/ahead_bazaar_child/ask_oracle - Copy.php
@@ -12,6 +12,16 @@ Template Name: Ask Oracle App
 jQuery("button").click(function(){
   jQuery("#div1").load("http://www.ask-oracle.com/charts/api.php?dt=2014-03-01T15%3A00%3A00%2B02%3A00");
 }); 
+
+/*
+var d = new Date();
+var jsondt = d.toJSON();
+var url = 'http://www.ask-oracle.com/charts/api.php?dt='+jsondt;
+var script = '<script type="text/javascript" src="';
+script += url+'"><' + '/script>';
+document.write(script);
+*/
+
 /*
 jQuery(document).ready(function(){
   jQuery("button").click(function(){
@@ -25,6 +35,32 @@ jQuery(document).ready(function(){
 var d = new Date();
 var n = d.toJSON();
 */
+
+/*jQuery.each(result, function(day_week_moth, day_week_moth_details){
+			
+	
+	jQuery(".result").append('<br /><b style="color:red;">'+day_week_moth+'</b>===============<br /><br />');
+	jQuery.each(day_week_moth_details, function(zodiac, zodiac_details){
+		jQuery(".result").append(zodiac_details.date+'-----------------<br />');
+		jQuery(".result").append('pisces :::::::::::<br />'+zodiac_details.content.pisces+'<br />');
+		jQuery(".result").append('aquarius :::::::::::<br />'+zodiac_details.content.aquarius+'<br />');
+		jQuery(".result").append('capricorn :::::::::::<br />'+zodiac_details.content.capricorn+'<br />');
+		jQuery(".result").append('sagittarius :::::::::::<br />'+zodiac_details.content.sagittarius+'<br />');
+		jQuery(".result").append('scorpio :::::::::::<br />'+zodiac_details.content.scorpio+'<br />');
+		jQuery(".result").append('libra :::::::::::<br />'+zodiac_details.content.libra+'<br />');
+		jQuery(".result").append('virgo :::::::::::<br />'+zodiac_details.content.virgo+'<br />');
+		jQuery(".result").append('leo :::::::::::<br />'+zodiac_details.content.leo+'<br />');
+		jQuery(".result").append('cancer :::::::::::<br />'+zodiac_details.content.cancer+'<br />');
+		jQuery(".result").append('gemini :::::::::::<br />'+zodiac_details.content.gemini+'<br />');
+		jQuery(".result").append('taurus :::::::::::<br />'+zodiac_details.content.taurus+'<br />');
+		jQuery(".result").append('aries :::::::::::<br />'+zodiac_details.content.aries+'<br /><br />');
+		jQuery(".result").append(JSON.stringify(result.daily));
+		
+	});
+	
+	
+  //jQuery(".result").append(i+'===============<br /><br />'+JSON.stringify(field) + " <br /><br />---------------------------------------------------------------------<br /><br />");
+});*/
 </script>
 </head>
 <body>
--- themes/ahead_bazaar_child/ask_oracle.php
+++ themes/ahead_bazaar_child/ask_oracle.php
@@ -6,35 +6,131 @@ Template Name: Ask Oracle App
 <!DOCTYPE html>
 <html>
 <head>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+<script src="http://code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+<script type="text/javascript">
+<?php $jsondata_url =  get_stylesheet_directory_uri().'/api.php';
+echo 'var api_ajax_url="'.$jsondata_url.'";';
+?>
+jQuery(document).ready(function(){
+//daily,daily-love,daily-career,weekly,weekly-career,weekly-love,monthly,monthly-love,monthly-career
+//pisces,aquarius,capricorn,sagittarius,scorpio,libra,virgo,leo,cancer,gemini,taurus,aries
+	jQuery.getJSON(api_ajax_url,function(result){
+		//jQuery(".result ul").append('<li>'+JSON.stringify(result['daily'][0].content.pisces)+'</li>');
+		
+		/*jQuery(".result ul").append('<h3>Daily Horoscope</h3>');
+		var daily_overview_id = 'daily_overview_pisces';
+		var daily_love_id = 'daily_love_pisces';
+		var daily_career_id = 'daily_career_pisces';
+		jQuery(".result").append('<div class="tabs">');
+		//Tabs
+		jQuery(".result").append('<ul class="daily">');
+		jQuery(".result ul.daily").append('<li><a href="#'+daily_overview_id+'">Overview</a></li>');
+		jQuery(".result ul.daily").append('<li><a href="#'+daily_love_id+'">Love</a></li>');
+		jQuery(".result ul.daily").append('<li><a href="#'+daily_career_id+'">Career</a></li>');
+		jQuery(".result").append('</ul>');
+		//Tabs Details
+		jQuery(".result").append('<div id="'+daily_overview_id+'">'+(result['daily'][0].content.pisces)+'</div>');
+		jQuery(".result").append('<div id="'+daily_love_id+'">'+(result['daily-love'][0].content.pisces)+'</div>');
+		jQuery(".result").append('<div id="'+daily_career_id+'">'+(result['daily-career'][0].content.pisces)+'</div>');
+		jQuery(".result").append('</div>');
+		
+		
+		jQuery(".result").append('<ul class="weekly">');
+		jQuery(".result").append('<li><h3>Weekly Horoscope</h3></li>');
+		jQuery(".result").append('<li>'+(result['weekly'][0].content.pisces)+'</li>');
+		jQuery(".result").append('<li>'+(result['weekly-career'][0].content.pisces)+'</li>');
+		jQuery(".result").append('<li>'+(result['weekly-love'][0].content.pisces)+'</li>');
+		jQuery(".result").append('</ul>');
+		
+		jQuery(".result").append('<ul class="monthly">');
+		jQuery(".result").append('<li><h3>Monthly Horoscope</h3></li>');
+		jQuery(".result").append('<li>'+(result['monthly'][0].content.pisces)+'</li>');
+		jQuery(".result").append('<li>'+(result['monthly-career'][0].content.pisces)+'</li>');
+		jQuery(".result").append('<li>'+(result['monthly-love'][0].content.pisces)+'</li>');
+		jQuery(".result").append('</ul>');
+		*/
+		
+		var zodiac_sign = 'pisces';
+		jQuery(".horoscope.tabs #tabs-daily-overview").html(result['daily'][0].content[zodiac_sign]);
+		jQuery(".horoscope.tabs #tabs-daily-love").html(result['daily-love'][0].content[zodiac_sign]);
+		jQuery(".horoscope.tabs #tabs-daily-career").html(result['daily-career'][0].content[zodiac_sign]);
+		
+		jQuery(".horoscope.tabs #tabs-weekly-overview").html(result['weekly'][0].content[zodiac_sign]);
+		jQuery(".horoscope.tabs #tabs-weekly-love").html(result['weekly-love'][0].content[zodiac_sign]);
+		jQuery(".horoscope.tabs #tabs-weekly-career").html(result['weekly-career'][0].content[zodiac_sign]);
+		
+		jQuery(".horoscope.tabs #tabs-monthly-overview").html(result['monthly'][0].content[zodiac_sign]);
+		jQuery(".horoscope.tabs #tabs-monthly-love").html(result['monthly-love'][0].content[zodiac_sign]);
+		jQuery(".horoscope.tabs #tabs-monthly-career").html(result['monthly-career'][0].content[zodiac_sign]);
+	});
+});
+
+</script>
 <script>
-var d = new Date();
-var jsondt = d.toJSON();
-var ajaxurl = "http://www.ask-oracle.com/charts/api.php?dt="+jsondt;
-jQuery.get( ajaxurl, function( data ) {
-	jQuery( ".result" ).html( data );
-	alert( "Load was performed." );
+$(function() {
+	$( ".tabs" ).tabs({
+		event: "mouseover"
+	});
 });
-
-
-/*
-$(document).ready(function(){
-  $("button").click(function(){
-	
-	$("#div1").load("http://www.ask-oracle.com/charts/api.php?dt=2014-03-01T15%3A00%3A00%2B02%3A00",function(responseTxt,statusTxt,xhr){
-      if(statusTxt=="success")
-        alert("External content loaded successfully!");
-      if(statusTxt=="error")
-        alert("Error: "+xhr.status+": "+xhr.statusText);
-    });
-  });
-});
-*/
 </script>
 </head>
 <body>
-<h2>Result will display here</h2>
-<div class="result"></div>
+<h3>Horoscope</h3>
+<div class="horoscope tabs">
+	<ul>
+		<li><a href="#tabs-daily">Daily Horoscope</a></li>
+		<li><a href="#tabs-weekly">Weekly Horoscope</a></li>
+		<li><a href="#tabs-monthly">Monthly Horoscope</a></li>
+	</ul>
+	<div id="tabs-daily">		
+		<div class="horoscope_daily tabs">
+			<ul>
+				<li><a href="#tabs-daily-overview">Overview</a></li>
+				<li><a href="#tabs-daily-love">Love</a></li>
+				<li><a href="#tabs-daily-career">Career</a></li>
+			</ul>
+			<div id="tabs-daily-overview">
+			</div>
+			<div id="tabs-daily-love">
+			</div>
+			<div id="tabs-daily-career">
+			</div>
+		</div>
+	</div>
+	<div id="tabs-weekly">
+		<div class="horoscope_weekly tabs">
+			<ul>
+				<li><a href="#tabs-weekly-overview">Overview</a></li>
+				<li><a href="#tabs-weekly-love">Love</a></li>
+				<li><a href="#tabs-weekly-career">Career</a></li>
+			</ul>
+			<div id="tabs-weekly-overview">
+			</div>
+			<div id="tabs-weekly-love">
+			</div>
+			<div id="tabs-weekly-career">
+			</div>
+		</div>
+	</div>
+	<div id="tabs-monthly">
+		<div class="horoscope_monthly tabs">
+			<ul>
+				<li><a href="#tabs-monthly-overview">Overview</a></li>
+				<li><a href="#tabs-monthly-love">Love</a></li>
+				<li><a href="#tabs-monthly-career">Career</a></li>
+			</ul>
+			<div id="tabs-monthly-overview">
+			</div>
+			<div id="tabs-monthly-love">
+			</div>
+			<div id="tabs-monthly-career">
+			</div>
+		</div>
+	</div>
+</div>
+
 
 </body>
 </html>
